41d52c46638a4946d9c6fe8ff2250ce038ecf2d6
sebastid <sebastien.durand71@gmail.com>
2015-08-05 13:40:19 +0100

    initial commit

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+#--- Merge the training and the test sets to create one data set.
+
+#--- Extract only the measurements on the mean and standard deviation for each measurement.
+
+#--- Use descriptive activity names to name the activities in the data set.
+
+#--- Appropriately labels the data set with descriptive variable name
+
+#--- From the data set in step4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
